df9df6da8203b7a7efd8dfccd4264d4dc41e13ec
abi2947 <captainphillips70@gmail.com>
2025-11-23 23:27:29 +0545

    Message schema defining how conversation taking between two users or in a room is being stored in database

---
+++ backend/models/Message.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "image", "file", "system"],
+      default: "text",
+    },
+    fileUrl: {
+      type: String,
+    },
+    fileName: {
+      type: String,
+    },
+    readBy: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+        readAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Message", messageSchema);
