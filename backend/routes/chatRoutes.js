380b3cdd5b6033c2082842c8df9e699e35d1ba46
abi2947 <captainphillips70@gmail.com>
2025-11-25 17:32:59 +0545

    Updated with image upload route

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -7,12 +7,25 @@ const {
   createPrivate,
   listsUsersChats,
   getMessages,
+  createGroup,
+  addParticipants,
+  removeParticipant,
+  markChatRead,
 } = require("../controllers/chatController");
 
 router.use(protect);
 
+// Private chat routes
 router.post("/create", createPrivate);
+
+// Group chat routes
+router.post("/create-group", createGroup);
+router.put("/:id/add-participants", addParticipants);
+router.put("/:id/remove-participant", removeParticipant);
+
+// General chat routes
 router.get("/", listsUsersChats);
+router.post("/:id/read", markChatRead);
 router.get("/:id/messages", getMessages);
 
 module.exports = router;
